afddc73e0b61f34eaa65f837e9ada031025f1876
Philihp Busby <philihp@gmail.com>
2013-11-16 17:08:22 -0500

    fixed ll

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 for name in *; do
   target="$HOME/.$name"
   if [ -e "$target" ]; then
+    echo $target
     if [ ! -L "$target" ]; then
       echo "WARNING: $target exists but is not a symlink."
     fi
